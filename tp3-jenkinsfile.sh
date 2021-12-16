6e330b0b5a508de2d1b77a81a01af6b70e81640d
Domingos SAFECA <safeca.domingos.93@gmail.com>
2021-12-16 06:44:06 -0800

    creation fichier jenkinsfile

---
+++ tp3-jenkinsfile.sh
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages {
+        stage('build') {
+            steps {
+                echo "Etape de build"
+                sh 'python2 --version'
+            }
+        }
+        stage ('Test') {
+            steps {
+                echo "Etape de test"
+            }
+        }
+        stage ('Deploy') {
+            steps {
+                echo "Etape de déploiement"
+            }
+        }
+    }
+}
